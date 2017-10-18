eabcc22d485393158e31c997a6cec22f23453ed5
Daniel Rammer <hamersaw@bushpath.com>
2017-10-18 14:30:26 -0600

    remove protobuf builder dependencies from datanode.

--- datanode/src/main/java/com/bushpath/anamnesis/datanode/HeartbeatManager.java
+++ datanode/src/main/java/com/bushpath/anamnesis/datanode/HeartbeatManager.java
@@ -1,29 +1,87 @@
 package com.bushpath.anamnesis.datanode;
 
+import org.apache.hadoop.hdfs.protocol.proto.DatanodeProtocolProtos;
+import org.apache.hadoop.hdfs.protocol.proto.HdfsProtos;
+import org.apache.hadoop.hdfs.protocol.proto.HdfsServerProtos;
+
 import com.bushpath.anamnesis.datanode.protocol.DatanodeClient;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class HeartbeatManager {
     private DatanodeClient client;
     private Configuration config;
+    private HdfsProtos.DatanodeIDProto datanodeIdProto;
 
     public HeartbeatManager(DatanodeClient client, Configuration config) {
         this.client = client;
         this.config = config;
+        this.datanodeIdProto = HdfsProtos.DatanodeIDProto.newBuilder()
+            .setIpAddr(config.ipAddr)
+            .setHostName(config.hostname)
+            .setDatanodeUuid(config.datanodeUuid)
+            .setXferPort(config.xferPort)
+            .setInfoPort(config.infoPort)
+            .setIpcPort(config.ipcPort)
+            .build();
 
         // send initial register message
-        client.registerDatanode(config.ipAddr, config.hostname, config.datanodeUuid,
-            config.xferPort, config.infoPort, config.ipcPort, 
-            config.namespceId, config.clusterId);
+
+        DatanodeProtocolProtos.RegisterDatanodeRequestProto req =
+            DatanodeProtocolProtos.RegisterDatanodeRequestProto.newBuilder()
+                .setRegistration(buildDatanodeRegistrationProto())
+                .build();
+
+        DatanodeProtocolProtos.RegisterDatanodeResponseProto response =
+            this.client.registerDatanode(req);
+
+        // TODO - handle response
 
         // start heartbeat timer
         Timer timer = new Timer(true);
         timer.scheduleAtFixedRate(new HeartbeatTask(client, config), 0, 5 * 1000);
     }
 
-    private static class HeartbeatTask extends TimerTask {
+    private DatanodeProtocolProtos.DatanodeRegistrationProto 
+        buildDatanodeRegistrationProto() {
+
+        HdfsServerProtos.StorageInfoProto storageInfoProto = 
+            HdfsServerProtos.StorageInfoProto.newBuilder()
+                .setLayoutVersion(0)
+                .setNamespceID(this.config.namespceId)
+                .setClusterID(this.config.clusterId)
+                .setCTime(System.currentTimeMillis())
+                .build();
+
+        HdfsServerProtos.BlockKeyProto currentKey =
+            HdfsServerProtos.BlockKeyProto.newBuilder()
+                .setKeyId(0)
+                .setExpiryDate(-1)
+                .build();
+
+        List<HdfsServerProtos.BlockKeyProto> allKeys = new ArrayList<>();
+
+        HdfsServerProtos.ExportedBlockKeysProto exportedBlockKeysProto =
+            HdfsServerProtos.ExportedBlockKeysProto.newBuilder()
+                .setIsBlockTokenEnabled(false)
+                .setKeyUpdateInterval(5000)
+                .setTokenLifeTime(5000)
+                .setCurrentKey(currentKey)
+                .addAllAllKeys(allKeys)
+                .build();
+
+        return DatanodeProtocolProtos.DatanodeRegistrationProto.newBuilder()
+                .setDatanodeID(this.datanodeIdProto)
+                .setStorageInfo(storageInfoProto)
+                .setKeys(exportedBlockKeysProto)
+                .setSoftwareVersion("2.8.0")
+                .build();
+    }
+
+    private class HeartbeatTask extends TimerTask {
         private DatanodeClient client;
         private Configuration config;
 
@@ -34,9 +92,19 @@ public class HeartbeatManager {
 
         @Override
         public void run() {
-            client.sendHeartbeat(this.config.ipAddr, this.config.hostname, 
-                this.config.datanodeUuid, this.config.xferPort, this.config.infoPort,
-                this.config.ipcPort, this.config.namespceId, this.config.clusterId);
+            List<HdfsProtos.StorageReportProto> reports = new ArrayList<>();
+            
+            DatanodeProtocolProtos.HeartbeatRequestProto req = 
+                DatanodeProtocolProtos.HeartbeatRequestProto.newBuilder()
+                    .setRegistration(buildDatanodeRegistrationProto())
+                    .addAllReports(reports)
+                    .build();
+
+            // send HeartbeatRequest
+            DatanodeProtocolProtos.HeartbeatResponseProto response =
+                this.client.sendHeartbeat(req);
+
+            // TODO - handle response
         }
     }
 }
--- datanode/src/main/java/com/bushpath/anamnesis/datanode/protocol/DatanodeClient.java
+++ datanode/src/main/java/com/bushpath/anamnesis/datanode/protocol/DatanodeClient.java
@@ -7,10 +7,6 @@ import org.apache.hadoop.hdfs.protocol.proto.DatanodeProtocolServiceGrpc;
 import org.apache.hadoop.hdfs.protocol.proto.HdfsProtos;
 import org.apache.hadoop.hdfs.protocol.proto.HdfsServerProtos;
 
-import com.bushpath.anamnesis.protocol.DatanodeProtocol;
-import com.bushpath.anamnesis.protocol.Hdfs;
-import com.bushpath.anamnesis.protocol.HdfsServer;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -18,10 +14,6 @@ import java.util.logging.Logger;
 public class DatanodeClient {
     private static final Logger logger = 
         Logger.getLogger(DatanodeClient.class.getName());
-    private static final String HDFS_VERSION = "2.8.0";
-    private static final int LAYOUT_VERSION = 0;
-    private static final boolean IS_BLOCK_TOKEN_ENABLED = false;
-    private static final long KEY_UPDATE_INTERVAL = 10000, TOKEN_LIFETIME = 30000;
         
     // grpc variables
     private final ManagedChannel channel;
@@ -38,80 +30,17 @@ public class DatanodeClient {
                         .build();
 
         this.blockingStub = DatanodeProtocolServiceGrpc.newBlockingStub(channel);
-
-        this.currentKeyID = 0;
     }
 
-    public void registerDatanode(String ipAddr, String hostName, String datanodeUuid,
-            int xferPort, int infoPort, int ipcPort, int namespceID, String clusterID) {
-        logger.info("registering datanode");
-        
-        // construct register request protobuf components
-        HdfsProtos.DatanodeIDProto datanodeIDProto = Hdfs.buildDatanodeIDProto(
-                ipAddr, hostName, datanodeUuid, xferPort, infoPort, ipcPort);
+    public DatanodeProtocolProtos.RegisterDatanodeResponseProto
+        registerDatanode(DatanodeProtocolProtos.RegisterDatanodeRequestProto req) {
 
-        HdfsServerProtos.StorageInfoProto storageInfoProto = 
-            HdfsServer.buildStorageInfoProto(this.LAYOUT_VERSION, namespceID, 
-                clusterID, System.currentTimeMillis());
-
-        HdfsServerProtos.BlockKeyProto currentKey = HdfsServer.buildBlockKeyProto(
-            this.currentKeyID, System.currentTimeMillis() + this.TOKEN_LIFETIME);
-
-        List<HdfsServerProtos.BlockKeyProto> allKeys = new ArrayList<>(); // TODO - fill
-
-        HdfsServerProtos.ExportedBlockKeysProto exportedBlockKeysProto =
-            HdfsServer.buildExportedBlockKeysProto(this.IS_BLOCK_TOKEN_ENABLED, 
-                this.KEY_UPDATE_INTERVAL, this.TOKEN_LIFETIME, currentKey, allKeys);
-
-        DatanodeProtocolProtos.DatanodeRegistrationProto datanodeRegistrationProto = 
-            DatanodeProtocol.buildDatanodeRegistrationProto( datanodeIDProto,
-                storageInfoProto, exportedBlockKeysProto, this.HDFS_VERSION);
-
-        DatanodeProtocolProtos.RegisterDatanodeRequestProto req =
-            DatanodeProtocol.buildRegisterDatanodeRequestProto(datanodeRegistrationProto);
-
-        // send RegisterDatanodeRequestProto
-        DatanodeProtocolProtos.RegisterDatanodeResponseProto response =
-            this.blockingStub.registerDatanode(req);
-
-        // TODO - handle response
+        return this.blockingStub.registerDatanode(req);
     }
 
-    public void sendHeartbeat(String ipAddr, String hostName, String datanodeUuid,
-            int xferPort, int infoPort, int ipcPort, int namespceID, String clusterID) {
-        logger.info("sending heartbeat");
-
-        // construct heartbeat request protobuf components
-        HdfsProtos.DatanodeIDProto datanodeIDProto = Hdfs.buildDatanodeIDProto(
-                ipAddr, hostName, datanodeUuid, xferPort, infoPort, ipcPort);
-
-        HdfsServerProtos.StorageInfoProto storageInfoProto = 
-            HdfsServer.buildStorageInfoProto(this.LAYOUT_VERSION, namespceID, 
-                clusterID, System.currentTimeMillis());
-
-        HdfsServerProtos.BlockKeyProto currentKey = HdfsServer.buildBlockKeyProto(
-            this.currentKeyID, System.currentTimeMillis() + this.TOKEN_LIFETIME);
-
-        List<HdfsServerProtos.BlockKeyProto> allKeys = new ArrayList<>(); // TODO - fill
-
-        HdfsServerProtos.ExportedBlockKeysProto exportedBlockKeysProto =
-            HdfsServer.buildExportedBlockKeysProto(this.IS_BLOCK_TOKEN_ENABLED, 
-                this.KEY_UPDATE_INTERVAL, this.TOKEN_LIFETIME, currentKey, allKeys);
-
-        DatanodeProtocolProtos.DatanodeRegistrationProto datanodeRegistrationProto = 
-            DatanodeProtocol.buildDatanodeRegistrationProto( datanodeIDProto,
-                storageInfoProto, exportedBlockKeysProto, this.HDFS_VERSION);
-
-        List<HdfsProtos.StorageReportProto> reports = new ArrayList<>();
+    public DatanodeProtocolProtos.HeartbeatResponseProto
+        sendHeartbeat(DatanodeProtocolProtos.HeartbeatRequestProto req) {
         
-        DatanodeProtocolProtos.HeartbeatRequestProto req = 
-            DatanodeProtocol.buildHeartbeatRequestProto(
-                    datanodeRegistrationProto, reports);
-
-        // send HeartbeatRequest
-        DatanodeProtocolProtos.HeartbeatResponseProto respnose =
-            this.blockingStub.sendHeartbeat(req);
-
-        // TODO - handle response
+        return this.blockingStub.sendHeartbeat(req);
     }
 }
